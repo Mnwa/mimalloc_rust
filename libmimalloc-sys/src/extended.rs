e844ea453e925845474db3f34451e790f3d46605
Mnwa <mihan@panfilov.biz>
2026-09-14 10:27:51 +0300

    Bump v

--- libmimalloc-sys/src/extended.rs
+++ libmimalloc-sys/src/extended.rs
@@ -564,7 +564,7 @@ pub const mi_option_max_segment_reclaim: mi_option_t = 21;
 #[cfg(feature = "v2")]
 pub const _mi_option_last: mi_option_t = 38;
 #[cfg(not(feature = "v2"))]
-pub const _mi_option_last: mi_option_t = 47;
+pub const _mi_option_last: mi_option_t = 48;
 
 unsafe extern "C" {
     // Note: mi_option_{enable,disable} aren't exposed because they're redundant
